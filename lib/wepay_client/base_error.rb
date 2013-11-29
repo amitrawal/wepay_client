80591485f650747af82abfdda152b57c128a7540
Udaya Kiran <udaykiran.vit@gmail.com>
2013-11-29 11:28:45 +0530

    Re-factoring exceptions - Created base_error.rb

---
+++ lib/wepay_client/base_error.rb
@@ -0,0 +1,15 @@
+class BaseError < StandardError
+
+  def initialize(message = '', code = nil)
+    @error_message = message
+    @error_code = code
+  end
+
+  def message
+    @error_message
+  end
+
+  def code
+    @error_code.to_s
+  end
+end
